c0a5adf9550b100d78f1a687101488ff25b9a3ac
Ralf Siewert (DCI) <ralf.siewert-freelancer@digitalcareerinstitute.org>
2022-05-10 14:16:34 +0200

    add PUBLIC_URL

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Test } from "./Test";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
       <Routes>
         <Route path="" element={<App />} />
         <Route path="test" element={<Test />} />
